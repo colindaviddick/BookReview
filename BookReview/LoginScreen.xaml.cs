7a6b0351be9df081ab46e224c906c257de423f64
COLIN\User <punkmuppet@gmail.com>
2019-04-02 01:01:29 +0100

    First Login & Dashboard created, some database stuff working

---
+++ BookReview/LoginScreen.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BookReview
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login.Content.ToString() == "Login")
+            {
+                //System.Windows.Forms.MessageBox.Show("Login");
+
+                // new MainWindow().Show();
+                #region Copied from old app
+
+                SqlConnection sqlCon = new SqlConnection(@"Data source=COLIN\SQLMAIN; Initial Catalog=TestLoginCredentials; Integrated Security=True;");
+
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    String query = "SELECT COUNT(1) FROM tblUser WHERE Username=@UserName AND Password=@Password";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.CommandType = CommandType.Text;
+                    sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                    if (count == 1)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Username or password was incorrect.");
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                    // this.Close();
+                }
+
+            }
+
+
+            #endregion
+
+            else if (Login.Content.ToString() == "Register")
+            {
+                // System.Windows.Forms.MessageBox.Show("Register");
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("I don't know what you've done.");
+            
+
+                //SqlConnection sqlCon = new SqlConnection(@"Data source=COLIN\SQLMAIN; Initial Catalog=TestLoginCredentials; Integrated Security=True;");
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChkRegister_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Login.Content = "Login";
+        }
+
+        private void ChkRegister_Checked(object sender, RoutedEventArgs e)
+        {
+            Login.Content = "Register";
+        }
+
+        private void Window_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (System.Windows.Forms.Control.IsKeyLocked(Keys.CapsLock))
+            {
+                CapsCheck.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // System.Windows.MessageBox.Show("The Caps Lock key is OFF.");
+                CapsCheck.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (System.Windows.Forms.Control.IsKeyLocked(Keys.CapsLock))
+            {
+                CapsCheck.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // System.Windows.MessageBox.Show("The Caps Lock key is OFF.");
+                CapsCheck.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
+
--- BookReview/MainWindow.xaml.cs
+++ BookReview/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿#region Can forget about this section for now.....
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace BookReview
 {
     /// <summary>
@@ -20,9 +25,217 @@ namespace BookReview
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SqlConnection sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = (@"Data Source=COLIN\SQLMAIN;Initial Catalog=TestLoginCredentials;Persist Security Info=True;User ID=sa;Password=Thr33four");
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowAuthors();
+            ShowBooks();
         }
+
+        #endregion
+
+        #region        // These Methods Need work
+
+        void AddBook(object sender, RoutedEventArgs e)
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+
+            try
+            {
+                // If Author already exists, get ID, add AuthorID + BookID to BookAuthor table. Lol
+                // Otherwise add new AuthorID & Book ID to table.
+                string authorQuery = "insert into Author values (@AuthorName); insert into Book values (@Title, @Synopsis)";
+
+                SqlCommand sqlCommand = new SqlCommand(authorQuery, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@AuthorName", newAuthor.Text);
+                sqlCommand.Parameters.AddWithValue("@Title", newBookTitle.Text);
+                sqlCommand.Parameters.AddWithValue("@Synopsis", newSynopsis.Text);
+                sqlCommand.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong..." + ex.ToString());
+
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowBooks();
+                ShowAuthors();
+            }
+        }
+
+
+        private void ListBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+
+
+                string queryTitle = "select Title from Book select Synopsis from Book where Id = @BookID";
+                //string queryAuthor = "select AuthorName from Author where Id = @AuthorID";
+
+                SqlCommand sqlCommand = new SqlCommand(queryTitle, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@BookId", listBooks.SelectedValue);
+                    sqlCommand.Parameters.AddWithValue("@Synopsis", listBooks.SelectedValue);
+
+                    DataTable bookIDTable = new DataTable();
+                    DataTable synopsisTable = new DataTable();
+
+                    sqlDataAdapter.Fill(bookIDTable);
+                    sqlDataAdapter.Fill(synopsisTable);
+
+                    bookTitle.Text = bookIDTable.Rows[0]["Title"].ToString();
+                    synopsis.Text = synopsisTable.Rows[0]["Synopsis"].ToString();
+
+
+                    // sqlCommand.Parameters.AddWithValue("@AuthorId", listBooks.SelectedValue);
+                    // DataTable authorIDTable = new DataTable();
+                    // sqlDataAdapter.Fill(authorIDTable);
+                    // bookTitle.Text = bookIDTable.Rows[0]["AuthorName"].ToString();
+                }
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("ShowSelectedAnimalInTextBox" + e2.ToString());
+            }
+        }
+
+
+        #endregion
+        #region       // Not working at all
+        private void Delete_Book(object sender, RoutedEventArgs e)
+        {
+            // Causing problems with Foreign Keys... Need to update BookAuthor table too??
+
+            try
+            {
+                string query = "delete from Book where Id = @Title";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Title", listBooks.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Delete Book" + ex.ToString());
+
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowBooks();
+            }
+        }
+
+        private void Delete_Author(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        #endregion
+
+        #region Can forget about these
+        // Perfect
+        void ShowAuthors()
+        {
+            try
+            {
+                string query = "SELECT * FROM Author ORDER BY AuthorName";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    var authorDataTable = new DataTable();
+                    sqlDataAdapter.Fill(authorDataTable);
+
+                    listAuthors.DisplayMemberPath = "AuthorName";
+                    listAuthors.SelectedValuePath = "Id";
+                    listAuthors.ItemsSource = authorDataTable.DefaultView;
+                }
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        }
+
+        void ShowBooks()
+        {
+            try
+            {
+                string query = "SELECT * FROM Book ORDER BY Title";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    var bookDataTable = new DataTable();
+                    sqlDataAdapter.Fill(bookDataTable);
+
+                    listBooks.DisplayMemberPath = "Title";
+                    listBooks.SelectedValuePath = "Id";
+                    listBooks.ItemsSource = bookDataTable.DefaultView;
+                }
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        }
+
+        private void ListAuthors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string query = "select * from Book b inner join BookAuthor ba on b.Id = ba.BookId where ba.AuthorId = @AuthorId";
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+
+                    sqlCommand.Parameters.AddWithValue("@AuthorId", listAuthors.SelectedValue);
+
+                    DataTable bookDataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(bookDataTable);
+
+                    //Which Information of the Table in DataTable should be shown in our ListBox?
+                    listBooks.DisplayMemberPath = "Title";
+                    //Which Value should be delivered, when an Item from our ListBox is selected?
+                    listBooks.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    listBooks.ItemsSource = bookDataTable.DefaultView;
+                }
+            }
+            catch (Exception e2)
+            {
+                // Causes some issues.
+
+                MessageBox.Show("ShowAssociatedAnimals" + e2.ToString());
+            }
+        }
+
     }
 }
+#endregion
